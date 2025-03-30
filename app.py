910a29b81067dc986ab5b7b87ff7fcd26cfeb192
hadiranweb <hadiranweb@gmail.com>
2025-03-30 13:06:01 +0330

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,6 @@ def translate_text_chunk(text, model_choice, delay=1):
         client = InferenceClient(
             model="deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
             token=os.getenv("HUGGINGFACE_TOKEN"))
-            timeout=500  # افزایش زمان انتظار
         prompt = f"متن زیر را به فارسی روان ترجمه کن:\n{text}"
         return client.text_generation(prompt, max_new_tokens=2000)
     else:
